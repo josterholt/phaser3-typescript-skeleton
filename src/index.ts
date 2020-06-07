21170ef1d543da985f30b9425c7980555e79029f
Justin Osterholt <shroder@gmail.com>
2020-06-06 19:54:31 -0700

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export function myTestFunction(): string {
+  // if (module.hot) {
+  //     module.hot.accept('./print.js', function() {
+  //     console.log('Accepting the updated printMe module!');
+  //     printMe();
+  //     })
+  // }
   return 'foo'
 }
+
+document.getElementById('wrapper').innerHTML = 'testing'
